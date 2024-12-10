a6200a9f42569298699f61ddb0c31ee95dc22ab7
Cooper Fiske-Kneafsey <Cooperjfk12@gmail.com>
2024-12-10 15:06:22 -0500

    added API call to newsAPI.org

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var json = "Loading..."
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Goodbye, world!")
+         Text(json)
         }
-        .padding()
+        .task {
+           func loadData() async {
+               let url = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2024-11-10&sortBy=publishedAt&apiKey=a9e0d3ccb1834334b6ef9bb15ba82b8e")!
+
+               let (data, _) = try! await URLSession.shared.data(from: url)
+               json = String(decoding: data, as: UTF8.self)
+           }
+           await loadData()
+        }
     }
 }
 
